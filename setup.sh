567d16b323d6c0fa1d78dc9da25ed38ff58e9fa8
botnyx <chlsbkrs@gmail.com>
2018-03-15 00:49:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 docker volume create --name=frontend-proxy-sslcerts
 docker volume create --name=frontend-proxy-vhosts
 docker volume create --name=frontend-proxy-html
-docker network create --name=frontend-proxy
+docker network create frontend-proxy
 docker-compose up -d
